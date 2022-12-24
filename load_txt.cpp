fcb758906d3b2fb4e0e008dac2ba9e05f609b4a8
Siddhant Solanki <siddhant@Siddhants-MacBook-Pro-2.local>
2022-12-24 12:06:04 -0500

    Fixed reader to get PRIMS in the correct order

--- load_txt.cpp
+++ load_txt.cpp
@@ -1,9 +1,9 @@
 #include "load_txt.hpp"
 
-ARRAY x,y,z;
-ARRAY rho,P,V1,V2,V3,B1,B2,B3,lfac,E1,E2,E3;
+// Global fields
 ARRAY Bsqr_sim,bfluid0_sim,bfluid1_sim,bfluid2_sim,bfluid3_sim;
-ARRAY b2_sim, rMKS_sim, thetaMKS_sim;
+ARRAY efluid0_sim,efluid1_sim,efluid2_sim,efluid3_sim;
+ARRAY b2_sim, e2_sim;
 
 ARRAY2D COORDS;
 ARRAY2D PRIMS;
@@ -18,6 +18,9 @@ void InitializeArrays(std::string FILE_NAME)
     COORDS.clear();
     PRIMS.clear();
 
+    ARRAY x,y,z;
+    ARRAY rho,P,V1,V2,V3,B1,B2,B3,lfac,E1,E2,E3;
+
     while (std::getline(infile, line))
     {
         // The first two lines form the user header
@@ -29,32 +32,39 @@ void InitializeArrays(std::string FILE_NAME)
                 row >> row_data[i];
             }
             
+            // Coordinates and prims
             x.push_back(row_data[0]);
             y.push_back(row_data[1]);
             z.push_back(row_data[2]);
-            rho.push_back(row_data[3]);
-            P.push_back(row_data[4]);
-            V1.push_back(row_data[5]);
-            V2.push_back(row_data[6]);
-            V3.push_back(row_data[7]);
-            B1.push_back(row_data[8]);
-            B2.push_back(row_data[9]);
-            B3.push_back(row_data[10]);
-            Bsqr_sim.push_back(row_data[11]);
-            bfluid0_sim.push_back(row_data[12]);
-            bfluid1_sim.push_back(row_data[13]);
-            bfluid2_sim.push_back(row_data[14]);
-            bfluid3_sim.push_back(row_data[15]);
-            lfac.push_back(row_data[16]);
-            b2_sim.push_back(row_data[17]);
-            thetaMKS_sim.push_back(row_data[18]);
-            rMKS_sim.push_back(row_data[19]);
+            V1.push_back(row_data[3]);
+            V2.push_back(row_data[4]);
+            V3.push_back(row_data[5]);
+            B1.push_back(row_data[6]);
+            B2.push_back(row_data[7]);
+            B3.push_back(row_data[8]);
+            E1.push_back(row_data[9]);
+            E2.push_back(row_data[10]);
+            E3.push_back(row_data[11]);
+            lfac.push_back(row_data[12]);
+            P.push_back(row_data[13]);
+            rho.push_back(row_data[14]);
+            // Extra arrays
+            Bsqr_sim.push_back(row_data[15]);
+            b2_sim.push_back(row_data[16]);
+            e2_sim.push_back(row_data[17]);
+            bfluid0_sim.push_back(row_data[18]);
+            bfluid1_sim.push_back(row_data[19]);
+            bfluid2_sim.push_back(row_data[20]);
+            bfluid3_sim.push_back(row_data[21]);
+            efluid0_sim.push_back(row_data[22]);
+            efluid1_sim.push_back(row_data[23]);
+            efluid2_sim.push_back(row_data[24]);
+            efluid3_sim.push_back(row_data[25]);
+
             // Normally read fields, set to 0 for now
-            if (!idealMHD)
+            if (idealMHD)
             {
-                E1.push_back(0.);
-                E2.push_back(0.);
-                E3.push_back(0.);
+                // Do something with the fields
             }
         }
         line_number ++;
@@ -77,7 +87,7 @@ void InitializeArrays(std::string FILE_NAME)
     PRIMS.push_back(lfac);
     PRIMS.push_back(P);
     PRIMS.push_back(rho);
-
+       
     int _vector_size = x.size();
     std::cout<<"Loaded the fluid data; number of cells = " << _vector_size << std::endl;
     return;
--- load_txt.hpp
+++ load_txt.hpp
@@ -3,7 +3,8 @@
 #include "defs.hpp"
 #endif
 
-const int NVARS = 20;
+const int NVARS = 26;
+
 
 void InitializeArrays(std::string FILE_NAME);
 extern void WriteVectorToFile(std::string fname, ARRAY2D data);
