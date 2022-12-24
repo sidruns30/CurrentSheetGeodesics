4bfd882b855a5a1464d6ab93e93c1766c0d2d6a5
Siddhant Solanki <siddhant@Siddhants-MacBook-Pro-2.local>
2022-12-23 23:57:03 -0500

    changed declarations

--- load_txt.hpp
+++ load_txt.hpp
@@ -1,13 +1,3 @@
-/*#include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <numeric>
-#include <algorithm>
-*/
 #ifndef DEF_HEAD
 #define DEF_HEAD (1)
 #include "defs.hpp"
@@ -15,7 +5,20 @@
 
 const int NVARS = 20;
 
+// Define types
+typedef std::vector <double> ARRAY;
+typedef std::vector <std::vector <double>> ARRAY2D;
+
+
+ARRAY x,y,z;
+ARRAY rho,P,V1,V2,V3,B1,B2,B3,lfac,E1,E2,E3;
+ARRAY Bsqr_sim,bfluid0_sim,bfluid1_sim,bfluid2_sim,bfluid3_sim;
+ARRAY b2_sim, rMKS_sim, thetaMKS_sim;
+
+// Necessary for field calculations
+const bool idealMHD = true;
+
 void InitializeArrays(std::string FILE_NAME);
 extern const int NVARS;
 
-extern void WriteVectorToFile(std::string fname, std::vector <std::vector <double>> data);
+extern void WriteVectorToFile(std::string fname, ARRAY2D data);
