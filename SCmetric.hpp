f77a44a054e94c1d149615ad570c2008065c084f
Siddhant Solanki <siddhant@Siddhants-MacBook-Pro-2.local>
2023-01-04 20:17:24 -0500

    moved old files

--- SCmetric.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DEF_HEAD
-#define DEF_HEAD (1)
-#include "defs.hpp"
-#endif
-
-void GetSCCovariantMetric(double g[NDIM][NDIM], double X[NDIM]);
-void GetSCContravariantMetric(double g[NDIM][NDIM], double X[NDIM]);
-void GetSCConnection(double X[NDIM], double gamma[NDIM][NDIM][NDIM]);
--- load_txt.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DEF_HEAD
-#define DEF_HEAD (1)
-#include "defs.hpp"
-#endif
-
-const int NVARS = 28;
-
-
-void InitializeArrays(ARRAY2D &COORDS, ARRAY2D &PRIMS, std::string FILE_NAME);
